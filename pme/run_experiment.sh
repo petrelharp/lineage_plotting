bd06e27fbe0d5ad5420e781c8ac0b2ef2fbc79e2
peter <petrel.harp@gmail.com>
2022-03-26 21:55:16 -0700

    .

--- pme/run_experiment.sh
+++ pme/run_experiment.sh
@@ -11,6 +11,7 @@ fi
 
 
 OUTDIR=$1
-slim -d "OUTDIR=\'${OUTDIR}\'" pme.slim &> $OUTDIR/slim.log
+slim -d "OUTDIR='${OUTDIR}'" -s 123 pme.slim &> $OUTDIR/slim.log
 
 
+python3 plot_density.py $OUTDIR/pme_123.trees
